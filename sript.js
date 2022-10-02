08b0812264f57b2cc576e015b9de0a791ac7f411
Angela-Amaro <aamaro@alumnae.mills.edu>
2022-10-02 15:48:48 -0500

    added functional button to start countdown to quiz, added an input for initials for after the quiz is finished, and added few comments for TODO's

--- sript.js
+++ sript.js
@@ -1,23 +1,29 @@
 var correct = document.querySelector(".answer");
 var incorrect = document.querySelector(".wronganswer");
 var time = document.querySelector(".time");
-var secondsLeft = 120;
+var secondsLeft = 60;
+var startbutton = document.querySelector(".start");
 
 function timer() {
+
     var timerInterval = setInterval(function () {
         secondsLeft--;
         time.textContent = "Time left: " + secondsLeft;
 
         if (secondsLeft === 0) {
             clearInterval(timerInterval);
-            sendMessage();
+            time.textContent = "You ran out of time!";
         }
     }, 1000);
 
 }
 
-function sendMessage() {
-    time.textContent = "You ran out of time!";
-}
+startbutton.addEventListener("click", timer);
+        //want to make start button disapear after being clicked
 
-timer();
+        //show first random question
+         //timer decreases when answer is wrong
+         
+        //game is over when timer reaches 0 or all questions answered
+
+        //local.storage for initials and score
