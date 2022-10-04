0736a4947036b25d89b63a9d9c57f5b59d0b886e
Angela-Amaro <aamaro@alumnae.mills.edu>
2022-10-04 01:54:43 -0500

    finished and met all criteria

--- README.md
+++ README.md
@@ -1,12 +1,31 @@
-# quiz
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and score
+
+# Angela's coding quiz
+
+## Description
+
+coding quiz
+
+## Snapshot
+ 
+[](./quiz.png)
+
+
+
+
+## process 
+
+I coded a quiz for coding people
+
+## languages
+
+HTML/JS
+
+## author
+
+Angela Amaro
+
+## Contact 
+
+[My GitHub](https://github.com/Angela-Amaro)
+
+[My Linkedin](https://www.linkedin.com/in/angela-amaro-342792204/)
--- sript.js
+++ sript.js
@@ -1,29 +1,96 @@
+var timerInterval;
 var correct = document.querySelector(".answer");
 var incorrect = document.querySelector(".wronganswer");
 var time = document.querySelector(".time");
-var secondsLeft = 60;
+var secondsLeft = 10;
 var startbutton = document.querySelector(".start");
+var currentIndex = 1;
 
 function timer() {
 
-    var timerInterval = setInterval(function () {
+    timerInterval = setInterval(function () {
         secondsLeft--;
         time.textContent = "Time left: " + secondsLeft;
+        startbutton.style.display = 'none';
 
         if (secondsLeft === 0) {
             clearInterval(timerInterval);
             time.textContent = "You ran out of time!";
+            endgame();
         }
     }, 1000);
 
 }
 
-startbutton.addEventListener("click", timer);
-        //want to make start button disapear after being clicked
+startbutton.addEventListener("click", startgame);
 
-        //show first random question
-         //timer decreases when answer is wrong
-         
-        //game is over when timer reaches 0 or all questions answered
+function questions(event) {
+    if (event.target.matches("input")) {
+        if (event.target.getAttribute("data-correct") != "answer") {
+            secondsLeft = secondsLeft - 5;
+        }
+        displaynextquestion();
+    }
 
-        //local.storage for initials and score
+}
+function displaynextquestion() {
+    if (currentIndex > 5) {
+        clearInterval(timerInterval);
+        endgame();
+    }
+    for (i = 1; i < 6; i++) {
+        var questionEl = document.getElementById("question" + i);
+        if (i == currentIndex) {
+            questionEl.addEventListener("click", questions);
+            questionEl.classList.add("show");
+            questionEl.classList.remove("hidden");
+        }
+        else {
+            questionEl.classList.remove("show");
+            questionEl.classList.add("hidden");
+        }
+    }
+    currentIndex++;
+}
+
+function startgame() {
+    timer();
+    displaynextquestion();
+
+}
+
+function endgame() {
+    var qsEl = document.getElementById("qs");
+    qsEl.classList.add("hidden");
+    var buttonEl = document.getElementById("score");
+    var endEl = document.getElementById("end");
+    endEl.classList.remove("hidden");
+    endEl.classList.add("show");
+    buttonEl.addEventListener("click", submitScore);
+    var spanEl = document.getElementById("totalscore");
+    spanEl.textContent = secondsLeft;
+    console.log(secondsLeft);
+    console.log(spanEl);
+}
+
+function submitScore() {
+    var inputEl = document.getElementById("init");
+    var roster = localStorage.getItem("roster");
+    var newScore = {
+        "init": inputEl.value,
+        "score": secondsLeft,
+    }
+    if (roster) {
+        roster = JSON.parse(roster);
+        roster.push(newScore);
+    }
+    else {
+        roster = [newScore]
+    }
+    localStorage.setItem("roster", JSON.stringify(roster));
+    console.log(inputEl.value);
+}
+
+
+
+        //local.storage for initials and score .parse andjson.stringify for arrays
